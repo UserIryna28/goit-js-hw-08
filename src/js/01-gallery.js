724e360fb0ed37c88778908240085c883ba1072d
UserIryna28 <Ltdcom@ukr.net>
2023-05-03 13:41:02 +0300

    Add styles simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
-// Описаний в документації
+
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
+
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Add imports above this line
