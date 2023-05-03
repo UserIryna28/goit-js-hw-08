db97654465ce96af15d6a232f24abada094acac3
UserIryna28 <Ltdcom@ukr.net>
2023-05-03 13:30:51 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,44 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
-console.log("Hello")
+
+const galleryContainer = document.querySelector(".gallery")
+const imgMarcup = createGalleryItems(galleryItems)
+
+galleryContainer.insertAdjacentHTML('beforeend', imgMarcup)
+// Реалізація делегування на ul.gallery
+galleryContainer.addEventListener("click", onGalleryContainerClick) 
+    
+
+function createGalleryItems(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join("");   
+}
+
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+
+})
+
+
+
